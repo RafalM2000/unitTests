35ce833bba3d0408515d2acf75f876c0cde28e16
Rafal <rmf@europe.com>
2020-06-03 06:37:36 +0200

    changes in unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UnitTests'`, () => {
+  it(`should have as title1 'Angular-In-Examples'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UnitTests');
+    expect(app.title1).toEqual('Angular-In-Examples');
   });
 
-  it('should render title', () => {
+  it('should render title2', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('UnitTests app is running!');
+    expect(compiled.querySelector('h2').textContent).toContain('app works');
   });
 });
